bba02025918a8d72b744a2a7b6905c8f35963433
saeame <saeame31@gmail.com>
2023-02-03 14:27:00 +0900

    상품 조회, 상품상세조회

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
-app.use(express.json());
-require("dotenv").config();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+require('dotenv').config();
+
+app.use(express.json());
+
+const router = require('./src/routes');
 
 app.listen(process.env.PORT, () => {
-    console.log(`http://127.0.0.1:${process.env.PORT}`);
+  console.log(`http://127.0.0.1:${process.env.PORT}`);
 });
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const ProductController = require('../controllers/product.controller.js');
+const ProductController = require('../controllers/product.controller');
 const productController = new ProductController();
 
 /* 전체 공개 */
@@ -12,10 +12,10 @@ router.get('/:productId', productController.getProduct);
 
 /* 관리자 권한 */
 //상품등록
-router.post('/admin', productController.addProduct);
-//상품수정
-router.patch('/admin/:productId', productController.editProduct);
-//상품삭제
-router.delete('/admin/:productId', productController.removeProduct);
+// router.post('/admin', productController.addProduct);
+// //상품수정
+// router.patch('/admin/:productId', productController.editProduct);
+// //상품삭제
+// router.delete('/admin/:productId', productController.removeProduct);
 
 module.exports = router;
